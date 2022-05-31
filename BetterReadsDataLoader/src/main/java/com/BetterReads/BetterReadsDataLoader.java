63508ede884af3a0d6b5496040d7a942f04a4060
Vipul Bhardwaj <vkb_vipul@hotmail.com>
2022-05-31 12:15:10 +0530

    First Commit

---
+++ BetterReadsDataLoader/src/main/java/com/BetterReads/BetterReadsDataLoader.java
@@ -0,0 +1,13 @@
+package com.BetterReads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetterReadsDataLoader {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetterReadsDataLoader.class, args);
+	}
+
+}
---
+++ BetterReadsDataLoader/src/test/java/com/BetterReads/BetterReadsDataLoaderTests.java
@@ -0,0 +1,13 @@
+package com.BetterReads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetterReadsDataLoaderTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
